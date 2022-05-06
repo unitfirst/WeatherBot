b876696869a62b249ea4833901ea65a440663a23
mattew <98488679+unitfirst@users.noreply.github.com>
2022-05-06 19:39:37 +0300

    -reload update after reset commit

--- Config.cs
+++ Config.cs
@@ -2,6 +2,10 @@ namespace WeatherBot
 {
     public class Config
     {
-        public static string Token { get; set; } = "5314975107:AAGYAQgQuWhDNslmuT-JH_-kQdGH6PJEoL0";
+        public static string Token
+        {
+            get => "5314975107:AAGYAQgQuWhDNslmuT-JH_-kQdGH6PJEoL0";
+            private set => Token = value;
+        }
     }
 }
