8689b0414af97e71bf0f64474bc79d0894461690
mattew <98488679+unitfirst@users.noreply.github.com>
2022-05-07 20:04:15 +0300

    -rename object in class

--- Weather/TemperatureInfo.cs
+++ Weather/TemperatureInfo.cs
@@ -5,6 +5,6 @@ namespace WeatherBot
     public class TemperatureInfo
     {
         public float Temp { get; set; }
-        public float FeelsLike { get; set; }
+        public float Feels_Like { get; set; }
     }
 }
