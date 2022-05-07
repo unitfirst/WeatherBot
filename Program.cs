17bdc7bec47e5690de4e05189254ecb8ef1dcc7f
mattew <98488679+unitfirst@users.noreply.github.com>
2022-05-07 16:24:36 +0300

    -update program.cs: add interface

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherBot
+﻿using System;
+
+namespace WeatherBot
 {
     internal class Program
     {
@@ -6,7 +8,31 @@
         {
             var client = new Client(Config.Token);
 
-            client.StartEcho();
+            Console.WriteLine("Welcome. Please select option:");
+            Console.WriteLine("0. Exit program");
+            Console.WriteLine("1. Start engine");
+
+            while (true)
+            {
+                var action = Console.ReadLine();
+                switch (action)
+                {
+                    case "0":
+                        Console.WriteLine("\nGoodbye.");
+                        return;
+                    case "1":
+                        client.StartEcho();
+                        break;
+                    default:
+                        Console.WriteLine("\nUnknown command! Try again.");
+                        Console.WriteLine("\nPlease select option:");
+                        Console.WriteLine("0. Exit program");
+                        Console.WriteLine("1. Start engine");
+                        continue;
+                }
+
+                break;
+            }
         }
     }
 }
